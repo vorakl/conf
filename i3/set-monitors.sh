15c9d382239959a757c00c97bc540821b42dca51
Oleksii Tsvietnov <me@vorakl.name>
2016-08-09 19:30:11 +0200

    Moved autostarted tools in i3 config

--- i3/set-monitors.sh
+++ i3/set-monitors.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 xrandr --auto
+xrandr --output eDP1 --off
 
 if xrandr | egrep '^HDMI1 connected' &>/dev/null
 then
-    xrandr --output HDMI1 --auto --primary --output eDP1 --off
+    xrandr --output HDMI1 --off
+    xrandr --output HDMI1 --auto --primary
 elif xrandr | egrep '^DP2-1 connected' &>/dev/null
 then
-    xrandr --output DP2-1 --auto --primary --output eDP1 --off
+    xrandr --output DP2-1 --off
+    xrandr --output DP2-1 --auto --primary
+elif xrandr | egrep '^DP2-2 connected' &>/dev/null
+then
+    xrandr --output DP2-2 --off
+    xrandr --output DP2-2 --auto --primary
 else
     xrandr --output eDP1 --auto --primary
 fi
